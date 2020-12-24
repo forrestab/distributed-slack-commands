333b1fab1e6169145b518f7f1e8975e4da3e5df7
Forrest Brown <forrestabrown@gmail.com>
2020-12-23 19:17:21 -0500

    feat: add "url_verification" slack event

---
+++ src/SlackBot.WebHooks.Receivers.Slack/SlackEvents.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlackBot.WebHooks.Receivers.Slack
+{
+    public static class SlackEvents
+    {
+        public const string URL_VERIFICATION = "url_verification";
+    }
+}
