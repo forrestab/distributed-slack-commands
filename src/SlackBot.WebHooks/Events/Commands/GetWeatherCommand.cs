4aa6fc6bc52335e3c85228b4835af26a2c5d1e0a
Forrest Brown <forrestabrown@gmail.com>
2021-01-13 21:53:38 -0500

    feat: publish `GetWeatherCommand` to the queue

--- src/SlackBot.WebHooks/Events/Commands/GetWeatherCommand.cs
+++ src/SlackBot.WebHooks/Events/Commands/GetWeatherCommand.cs
@@ -1,13 +1,19 @@
+using NServiceBus;
 using System.CommandLine;
 using System.CommandLine.Invocation;
+using System.Threading.Tasks;
 
 namespace SlackBot.WebHooks.Events.Commands
 {
     public class GetWeatherCommand : Command
     {
-        public GetWeatherCommand()
+        private readonly IEndpointInstance endpoint;
+
+        public GetWeatherCommand(IEndpointInstance endpoint)
             : base("weather", "Check weather for provided zipcode.")
         {
+            this.endpoint = endpoint;
+
             this.AddOption(new Option<int>(new string[] { "--zip", "-z" })
             {
                 IsRequired = true
@@ -15,13 +21,10 @@ namespace SlackBot.WebHooks.Events.Commands
             this.Handler = CommandHandler.Create<GetWeatherOptions>(HandleCommand);
         }
 
-        private void HandleCommand(GetWeatherOptions options)
-        {
-            
-        }
+        private async Task HandleCommand(GetWeatherOptions options) => await this.endpoint.Publish(options);
     }
 
-    public class GetWeatherOptions
+    public class GetWeatherOptions : IEvent
     {
         public int Zip { get; }
 
